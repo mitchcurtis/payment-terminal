afdea8c2ce162c2d28c483cd63d8317e51c9b24f
Mitch Curtis <mitch.curtis@theqtcompany.com>
2016-01-28 13:47:05 +0100

    Parse messages according to specifications

--- licenseplatemodel.cpp
+++ licenseplatemodel.cpp
@@ -10,10 +10,10 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT receiveMessageCallback(IOTHUB_MESSAGE_HA
     const char* buffer;
     size_t size;
     if (IoTHubMessage_GetByteArray(message, (const unsigned char**)&buffer, &size) == IOTHUB_MESSAGE_OK)
-        qDebug("Received Message with Data: <<<%.*s>>> & Size=%d\r\n", (int)size, buffer, (int)size);
+        qDebug("Received Message with Data: <<<%.*s>>> & Size=%d", (int)size, buffer, (int)size);
 
-    // TODO: fix string
-    QString messageStr(buffer);
+    // TODO: conversion?
+    QString messageStr(QString::fromUtf8(buffer, size));
     // Ensure that the invokable function is called in a thread-safe manner via QueuedConnection.
     QMetaObject::invokeMethod(model, "onMessageReceived", Qt::QueuedConnection, Q_ARG(QString, messageStr));
     return IOTHUBMESSAGE_ACCEPTED;
@@ -67,9 +67,68 @@ int LicensePlateModel::columnCount(const QModelIndex &) const
     return 1;
 }
 
+enum LicensePlateAction
+{
+    UnknownAction,
+    LicensePlateAdded,
+    LicensePlateRemoved,
+};
+
+bool parseLicensePLateNumber(const QString &message, QString &licensePlateNumber)
+{
+    int equalsIndex = message.indexOf(QLatin1Char('='));
+    if (equalsIndex == -1) {
+        qWarning() << "Malformed message; expected '=' before license plate number:" << message;
+        return false;
+    }
+
+    QString licensePlateStr = message.mid(equalsIndex + 1);
+    if (licensePlateStr.isEmpty()) {
+        qWarning() << "Empty license plate number";
+        return false;
+    }
+
+    licensePlateNumber = licensePlateStr;
+    return true;
+}
+
+LicensePlateAction extractLicenseData(const QString &message, QString &licensePlateNumber)
+{
+    if (message.startsWith(QStringLiteral("LPA")) && parseLicensePLateNumber(message, licensePlateNumber)) {
+        return LicensePlateAdded;
+    } else if (message.startsWith(QStringLiteral("LPR")) && parseLicensePLateNumber(message, licensePlateNumber)) {
+        return LicensePlateRemoved;
+    }
+
+    qWarning() << "Unknown license plate action:" << message;
+    return UnknownAction;
+}
+
 void LicensePlateModel::onMessageReceived(const QString &message)
 {
-    beginInsertRows(QModelIndex(), mPlates.size(), mPlates.size());
-    mPlates.append(message);
-    endInsertRows();
+    QString licensePlateNumber;
+    LicensePlateAction action = extractLicenseData(message, licensePlateNumber);
+    if (action == UnknownAction)
+        return;
+
+    if (action == LicensePlateAdded) {
+        if (mPlates.contains(licensePlateNumber)) {
+            qWarning() << "License plate number" << licensePlateNumber << "already exists in model";
+            return;
+        }
+
+        beginInsertRows(QModelIndex(), mPlates.size(), mPlates.size());
+        mPlates.append(licensePlateNumber);
+        endInsertRows();
+    } else if (action == LicensePlateRemoved) {
+        int index = mPlates.indexOf(licensePlateNumber);
+        if (index == -1) {
+            qWarning() << "License plate number" << licensePlateNumber << "doesn't exist in model";
+            return;
+        }
+
+        beginRemoveRows(QModelIndex(), index, index);
+        mPlates.remove(index);
+        endRemoveRows();
+    }
 }
