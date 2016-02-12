ebcf5c2226ee37327500b98355258e96491273a0
Mitch Curtis <mitch.curtis@theqtcompany.com>
2016-02-12 13:11:24 +0100

    Add empty german translation, make project work with lupdate

--- payment-terminal.pro
+++ payment-terminal.pro
@@ -20,6 +20,10 @@ SOURCES += main.cpp \
 RESOURCES += qml.qrc \
     images.qrc
 
+lupdate_only {
+    SOURCES = *.qml
+}
+
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH =
 
---
+++ payment-terminal_de.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>CardPromptScreen</name>
+    <message>
+        <location filename="CardPromptScreen.qml" line="30"/>
+        <source>Paying </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="CardPromptScreen.qml" line="40"/>
+        <source>Please place your card on the card reader</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmationScreen</name>
+    <message>
+        <location filename="ConfirmationScreen.qml" line="33"/>
+        <source>Payment successful. Thank you!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConfirmationScreen.qml" line="51"/>
+        <source>Vehicle:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConfirmationScreen.qml" line="71"/>
+        <source>Amount paid:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConfirmationScreen.qml" line="91"/>
+        <source>Parking place:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LanguageScreen</name>
+    <message>
+        <location filename="LanguageScreen.qml" line="18"/>
+        <source>Pay your parking fee here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LanguageScreen.qml" line="26"/>
+        <source>Select Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NavigationBar</name>
+    <message>
+        <location filename="NavigationBar.qml" line="46"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="NavigationBar.qml" line="76"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PaymentSummaryScreen</name>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="29"/>
+        <source>Pay for the vehicle:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="45"/>
+        <source>Parking time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="51"/>
+        <source>%L1h %L2m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="64"/>
+        <source>Amount to pay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="70"/>
+        <source>%L1 €</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="77"/>
+        <source>We accept credit card payments only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PaymentSummaryScreen.qml" line="94"/>
+        <source>Pay Now</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlateSelectionScreen</name>
+    <message>
+        <location filename="PlateSelectionScreen.qml" line="15"/>
+        <source>Select your license plate number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReadingCardScreen</name>
+    <message>
+        <location filename="ReadingCardScreen.qml" line="34"/>
+        <source>Paying %L1 €</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ReadingCardScreen.qml" line="44"/>
+        <source>Reading your card...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
